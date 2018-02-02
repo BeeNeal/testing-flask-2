c0289db2dabb554c56dcefdff429c0d26e6d46ca
Hackbright Student <no-reply@hackbrightacademy.com>
2018-02-02 12:58:56 -0800

    Cleaning up comments.

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+import flask
 
 from party import app
 from model import db, example_data, connect_to_db
@@ -16,8 +17,6 @@ class PartyTests(unittest.TestCase):
         self.assertIn("board games, rainbows, and ice cream sundaes", result.data)
 
     def test_no_rsvp_yet(self):
-        # FIXME: Add a test to show we see the RSVP form, but NOT the
-        # party details
         result = self.client.get("/")
         self.assertIn("Oooh! I want to come!", result.data)
         self.assertNotIn("123 Magic Unicorn Way", result.data)
@@ -30,7 +29,6 @@ class PartyTests(unittest.TestCase):
         self.assertNotIn("Oooh! I want to come!", result.data)
         self.assertIn("123 Magic Unicorn Way", result.data)
 
-#No tear down? Not needed when using 'with'?
 
 class PartyTestsDatabase(unittest.TestCase):
     """Flask tests that use the database."""
@@ -41,6 +39,8 @@ class PartyTestsDatabase(unittest.TestCase):
         self.client = app.test_client()
         app.config['TESTING'] = True
 
+        # The second 'with' STORES a session in the test browser
+        # (so that it can be fake-logged-in)
         with self.client as c:
             with c.session_transaction() as sess:
                 sess['RSVP'] = True
